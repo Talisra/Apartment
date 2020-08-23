0edfc8b98b624c907dad76d3d820f280a0bd35cd
Talisra <talisraeli.t@gmail.com>
2020-08-23 15:27:13 +0300

    doors
    doors can be now opened

---
+++ Assets/Scripts/opendoor.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class opendoor : MonoBehaviour
+{
+    private Animator _animator;
+    private bool isOpen = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            _animator.SetTrigger("Open");
+            isOpen = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (isOpen && other.gameObject.tag == "Player")
+        {
+            _animator.SetTrigger("Close");
+            isOpen = false;
+        }
+    }
+}
